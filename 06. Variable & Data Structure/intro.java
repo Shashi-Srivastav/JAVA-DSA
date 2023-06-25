baf3c060e5020a8c880f8c5bdddc0f453d523ea1
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-25 20:38:29 +0530

    intro

---
+++ 06. Variable & Data Structure/intro.java
@@ -0,0 +1,7 @@
+public class intro{
+    public static void main(String[] args){
+        System.out.print("Hello World\n");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
