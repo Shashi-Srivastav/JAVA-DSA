7a92746bd04b25a7c3217da6017f302b491020b3
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-25 20:38:34 +0530

    pattern

---
+++ 06. Variable & Data Structure/Pattern.java
@@ -0,0 +1,9 @@
+public class Pattern{
+    public static void main(String[] arg){
+        System.out.print("* * * * *\n");
+        System.out.print("* * * *\n");
+        System.out.print("* * *\n");
+        System.out.print("* *\n");
+        System.out.print("*");
+    }
+}
