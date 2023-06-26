e90c0dc22ca9fd04d0622990f889fc2548ec1370
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-26 17:51:05 +0530

    variable

---
+++ 06. Variable & Data Structure/Variable.java
@@ -0,0 +1,10 @@
+public class Variable {
+    public static void main(String arg[]){
+        int age = 18;
+        String name = "shashi";
+        System.out.println(age);
+        System.out.println(name);
+        age = 22;
+        System.out.println(age);
+    }
+}
