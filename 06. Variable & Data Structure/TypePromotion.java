c4d000ecc94eab08c383ca4feeb8c1aa47bd3eeb
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-26 17:50:57 +0530

    type promotion

---
+++ 06. Variable & Data Structure/TypePromotion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class TypePromotion {
+    public static void main(String args[]){
+        char a = 'a';
+        char b = 'b';
+        System.out.println(b-a);
+        int x = 10;
+        float y = 33.33f;
+        double z = 3000;
+       double ans = x + y + z;
+       System.out.println(ans);
+
+    }
+}
