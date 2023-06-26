360c21813663bb13ad6c7695356f8c9580c9a395
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-26 17:51:17 +0530

    UNIRAYOP

---
+++ 07. Operators/UnaryOperators.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class UnaryOperators {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 12;
+        int b = 12;
+        System.out.println(++a);
+    }
+}
