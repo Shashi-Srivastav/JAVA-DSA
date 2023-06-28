64a316acd6ce6131b8d3edb3a604ec7c7242686e
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-28 18:17:24 +0530

    do while

---
+++ 9. Loops (Flow Control)/Do_While.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Do_While {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        do{
+            System.out.println("Hi");
+            n++;
+        }while(n<=10);
+    }
+}
