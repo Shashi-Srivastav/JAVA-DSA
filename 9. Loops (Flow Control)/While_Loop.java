57d77f1fd0e667ab8827e33f184485cd9307eb89
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-28 18:17:47 +0530

    while lop

---
+++ 9. Loops (Flow Control)/While_Loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class While_Loop {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        while(a<11){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
