73a3fbb900c5229615378e653c03b75d01bd1544
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-06-30 21:41:22 +0530

    intro

---
+++ 11. Functions & Methods/intro.java
@@ -0,0 +1,9 @@
+public class intro {
+    public static void print(){
+        System.out.println("Hello World");
+    }
+    public static void main(String arhs[]){
+        print();
+    }
+    
+}
