136daced10553a1336527b24ce6b1636f003d3ae
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-02 19:14:06 +0530

    overloading

--- 11. Functions & Methods/Overloading_using_parameter.java
+++ 11. Functions & Methods/Overloading_using_parameter.java
@@ -1,4 +1,14 @@
 import java.util.*;
 public class Overloading_using_parameter {
-    public static void main()
+    public static int sum (int a, int b){
+        return a+b;
+    }
+    public static int sum (int a,int b,int c){
+        return a+b+c;
+    }
+    public static void main(){
+        System.out.println(sum(2,3));
+
+        System.out.println(sum(2,3,4));
+    }
 }
