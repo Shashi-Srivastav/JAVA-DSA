15af61df53356917bea2485a6c4780df069d3910
Shashi Srivastava <Shashisrivastav96@gmail.com>
2023-07-02 19:14:14 +0530

    bin to dec

---
+++ 11. Functions & Methods/Binary_to_decimal.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Binary_to_decimal{
+    public static int Bin_to_Des(int Bin){
+        
+        return 
+    }
+    public static void main(String args[]){
+
+    }
+}
